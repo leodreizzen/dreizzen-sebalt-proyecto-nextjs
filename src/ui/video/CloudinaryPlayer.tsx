3cfec6fb686b46bec056b80e24b2cc1cacbceaff
leodreizzen <leodreizzen@gmail.com>
2024-05-17 03:00:26 -0300

    Arreglar Vercel

--- src/ui/video/CloudinaryPlayer.tsx
+++ src/ui/video/CloudinaryPlayer.tsx
@@ -1,4 +1,5 @@
 import { VideoDTO } from "@/data/DTO";
+import { ReactNode } from "react";
 
 export default function CloudinaryPlayer({video, className, autoPlay=false, active=true}: {video: VideoDTO, className?: string | undefined, autoPlay?: boolean, active?:boolean}): ReactNode{
     return(
