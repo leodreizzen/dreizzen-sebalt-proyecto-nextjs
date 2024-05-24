6f3904ddd9cd9a811eee022ef97d0465581340e4
Ian Sebalt <iansebalt@live.com.ar>
2024-05-24 15:35:17 -0300

    Mejorado aspecto responsive de la card de producto destacado.

--- src/ui/cards/FeaturedProductCard.tsx
+++ src/ui/cards/FeaturedProductCard.tsx
@@ -9,7 +9,9 @@ import { Chip } from "@nextui-org/chip";
 import AddToCartButton from "../AddToCartButton";
 import { useRouter } from "next/navigation";
 
-export default function FeaturedProductCard({ product, className }: { product: ProductDTO, className?: string }) {
+export default function FeaturedProductCard({ product, className }: { product: ProductDTO, className?: string, size: string }) {
+
+
     const router = useRouter();
     function handleCardPress() {
         router.push(`/product/${product.id}`)
@@ -17,7 +19,7 @@ export default function FeaturedProductCard({ product, className }: { product: P
     
     return (
         <>
-        <div className={clsx("gap-3", className)}>
+        <div className={clsx("gap-3 @container", className)}>
         <Card className={"w-full h-full"} onPress={handleCardPress} isPressable isFooterBlurred>
             <CardBody className="gap-3 aspect-video">
                 <Image src={product.coverImage.url} alt={product.coverImage.alt} fill={true}/>
@@ -25,16 +27,16 @@ export default function FeaturedProductCard({ product, className }: { product: P
             <CardFooter className="bg-content-1 border-default-600 dark:border-default-100 h-1/6">
                 <div className="flex flex-grow gap-2 items-center">
                     <div className="flex flex-col justify-start items-start">
-                        <div className="flex gap-2">
-                            <Chip size="md" color="primary" className="text-white">Deportes</Chip>
-                            <Chip size="md" color="primary" className="text-white">Futbol</Chip>
-                            <Chip size="md" color="primary" className="text-white">Femenino</Chip>
+                        <div className="flex flex-wrap gap-2 h-[30px] overflow-y-hidden mt-1">
+                            <Chip size="md" color="primary" className="text-white text-tiny  @2xl:text-sm" classNames = {{ base: "p-0"}}>Deportes</Chip>
+                            <Chip size="md" color="primary" className="text-white text-tiny  @2xl:text-sm">Futbol</Chip>
+                            <Chip size="md" color="primary" className="text-white text-tiny  @2xl:text-sm">Femenino</Chip>
                         </div>
-                        <p className="text-white font-bold text-4xl mt-1">{product.name}</p>
+                        <p className="text-white font-bold @2xl:mt-1 @xl:text-large @2xl:text-4xl text-start">{product.name}</p>
                     </div>
                 </div>
-                <p className="text-white mr-4 text-large">{formatPrice(product.currentPrice_cents)}</p>
-                <AddToCartButton/>
+                <p className="text-white mr-4 sm:text-large">{formatPrice(product.currentPrice_cents)}</p>
+                <AddToCartButton className="text-black pr-1" textClassName="hidden @xl:block"/>
 
             </CardFooter>
        </Card>
