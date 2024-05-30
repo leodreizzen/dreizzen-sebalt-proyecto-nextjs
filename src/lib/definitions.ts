5d33061377c0c1e0cbfef6f3b7cb06a39a54db05
Leonardo Dreizzen <64935571+leodreizzen@users.noreply.github.com>
2024-05-30 19:14:15 -0300

    Cambios en DB

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -1,1 +1,25 @@
+import { Company, Image, Product, Tag, Video, Purchase, InvoiceData } from "@prisma/client";
+
 export type ShoppingCart = number[];
+
+export interface ProductWithTags extends Product{
+    tags: Tag[]
+}
+
+export interface ProductWithCoverImage extends Product{
+    coverImage: Image
+}
+
+export interface ProductWithMedia extends ProductWithCoverImage{
+    descriptionImages: Image[]
+    videos: Video[]
+}
+
+export interface ProductWithcCompanies extends Product{
+    developer: Company
+    publisher: Company
+}
+
+export interface PurchaseWithInvoiceData extends Purchase{
+    invoiceData: InvoiceData
+}
