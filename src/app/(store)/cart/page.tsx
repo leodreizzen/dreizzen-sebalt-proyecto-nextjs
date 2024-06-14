273193c4fddaf06dd46397c53959eff7a0740e54
leodreizzen <leodreizzen@gmail.com>
2024-06-14 02:51:04 -0300

    Arreglar página de carrito en Firefox

--- src/app/(store)/cart/page.tsx
+++ src/app/(store)/cart/page.tsx
@@ -1,22 +1,22 @@
 import MarqueeOnOverflow from "@/ui/MarqueeOnOverflow";
-import { formatPrice } from "@/util/formatUtils"
-import { Button } from "@nextui-org/button";
+import {formatPrice} from "@/util/formatUtils"
+import {Button} from "@nextui-org/button";
 import Image from "next/image"
 import Link from "next/link";
 import clsx from "clsx";
-import { BsCartX } from "react-icons/bs";
+import {BsCartX} from "react-icons/bs";
 import {fetchCartProducts} from "@/lib/data";
 import {ProductWithCoverImage} from "@/lib/definitions";
 import RemoveFromCartPageButton from "@/ui/video/RemoveFromCartPageButton";
-
-export default async function CartPage({ }) {
+import {Link as NextUILink} from "@nextui-org/link";
+export default async function CartPage({}) {
     const products = await fetchCartProducts()
     return (
         <main className="flex flex-col items-center min-h-full">
             <h1 className="text-center text-3xl font-bold my-3 h-full">Carrito</h1>
             {products.length > 0 ?
-                <CartWithItemsPage products={products} className="w-full sm:w-10/12 gap-4 mb-4 sm:px-4" />
-                : <EmptyCartPage className="w-full sm:w-10/12 gap-4 mb-4 sm:px-4" />
+                <CartWithItemsPage products={products} className="w-full sm:w-10/12 gap-4 mb-4 sm:px-4"/>
+                : <EmptyCartPage className="w-full sm:w-10/12 gap-4 mb-4 sm:px-4"/>
             }
 
 
@@ -24,7 +24,7 @@ export default async function CartPage({ }) {
     )
 }
 
-function CartWithItemsPage({ products, className }: { products: ProductWithCoverImage[], className?: String }) {
+function CartWithItemsPage({products, className}: { products: ProductWithCoverImage[], className?: String }) {
     const total = products.reduce((acc, product) => acc + product.currentPrice_cents, 0);
 
     return (
@@ -33,13 +33,21 @@ function CartWithItemsPage({ products, className }: { products: ProductWithCover
                 {
                     products.map(
                         product => (
-                            <div className="border border-borders grid grid-cols-[auto_auto] xs:grid-cols-[auto_1fr_auto_auto] grid-flow-row grid-rows-[auto_min-content_min-content] xs:grid-rows-1 xs:h-16 sm:h-20 pr-2 py-2 xs:py-0  sm:rounded-lg overflow-clip" key={product.id}>
-                                <div className="relative max-xs:w-5/12 xs:h-full aspect-video max-xs:col-span-2 max-xs:rounded-xl max-xs:overflow-clip  max-xs:justify-self-center">
-                                    <Link href={`/product/${product.id}`}><Image src={product.coverImage.url} alt={product.coverImage.alt} fill /></Link>
+                            <div
+                                className="border border-borders grid grid-cols-[auto_auto] xs:grid-cols-[auto_1fr_auto_auto] grid-flow-row grid-rows-[auto_min-content_min-content] xs:grid-rows-1 xs:h-16 sm:h-20 pr-2 py-2 xs:py-0  sm:rounded-lg overflow-clip"
+                                key={product.id}>
+                                <div
+                                    className="max-xs:w-5/12 xs:h-full max-xs:col-span-2 max-xs:rounded-xl max-xs:overflow-clip  max-xs:justify-self-center">
+                                    <Link href={`/product/${product.id}`} className="size-full aspect-video relative inline-block">
+                                        <Image src={product.coverImage.url} alt={product.coverImage.alt} fill/>
+                                    </Link>
                                 </div>
-                                <div className="flex items-center min-w-0 mx-2 max-xs:mt-1 max-xs:col-span-2 max-xs:justify-self-center">
-                                    <MarqueeOnOverflow direction="horizontal" animation={["animate-marqueeX", "animate-marqueeX2"]}>
-                                        <Link href={`/product/${product.id}`}><span className="ml-1 mr-1 align-middle text-nowrap">{product.name}</span></Link>
+                                <div
+                                    className="flex items-center min-w-0 mx-2 max-xs:mt-1 max-xs:col-span-2 max-xs:justify-self-center">
+                                    <MarqueeOnOverflow direction="horizontal"
+                                                       animation={["animate-marqueeX", "animate-marqueeX2"]}>
+                                        <NextUILink color="foreground" as={Link} href={`/product/${product.id}`}><span
+                                            className="ml-1 mr-1 align-middle text-nowrap">{product.name}</span></NextUILink>
                                     </MarqueeOnOverflow>
                                 </div>
                                 <div className="flex items-center xs:mr-1  max-xs:justify-self-end">
@@ -54,17 +62,19 @@ function CartWithItemsPage({ products, className }: { products: ProductWithCover
             <div className="flex self-end text-lg items-center mx-3">
                 <span>Total:</span>
                 <span className="pl-2 pr-4">{formatPrice(total)}</span>
-                <Button as={Link} href="/purchase" className="font-bold bg-transparent text-foreground border-foreground border" size="md">Comprar ahora</Button>
+                <Button as={Link} href="/purchase"
+                        className="font-bold bg-transparent text-foreground border-foreground border" size="md">Comprar
+                    ahora</Button>
             </div>
         </div>
     )
 }
 
-function EmptyCartPage({ className }: { className?: String }) {
+function EmptyCartPage({className}: { className?: String }) {
     return (
         <div className={clsx(className, "flex flex-col items-center h-full")}>
             <div className="aspect-square w-1/5">
-                <BsCartX className="size-full" />
+                <BsCartX className="size-full"/>
             </div>
             <p className="pt-3 text-center">Tu carrito está vacío. Agrega productos para comprarlos</p>
             <Button as={Link} href="/" className="text-center bg-primary text-foreground mt-2">Elegir productos</Button>
